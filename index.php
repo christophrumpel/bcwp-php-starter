e41832818d1101cc25058a2d96b9cb3dc535c5a8
Christoph Rumpel <c.rumpel@kabsi.at>
2018-07-02 17:46:13 +0200

    Add state code for 03

--- index.php
+++ index.php
@@ -10,14 +10,14 @@ $dotenv->load();
 $log = new Logger('general');
 $log->pushHandler(new StreamHandler('debug.log'));
 
-$input = json_decode(file_get_contents('php://input'), true);
-$log->info('Request: ', $input ?? []);
+$input = json_decode(file_get_contents('php://input'), true) ?? $_GET;
+$log->info('Request: ', $input);
 
-if (isset($_REQUEST['hub_challenge'])) {
+if (isset($input['hub_challenge'])) {
 
-    $fbHubMode = $_GET['hub_mode'];
-    $fbHubVerifyToken = $_GET['hub_verify_token'];
-    $fbHubChallenge = $_GET['hub_challenge'];
+    $fbHubMode = $input['hub_mode'];
+    $fbHubVerifyToken = $input['hub_verify_token'];
+    $fbHubChallenge = $input['hub_challenge'];
 
     if ($fbHubMode === 'subscribe' && $fbHubVerifyToken === getenv('FACEBOOK_VERIFY_TOKEN')) {
         $log->info('Inside verify method');
@@ -27,29 +27,33 @@ if (isset($_REQUEST['hub_challenge'])) {
     exit;
 }
 
-$telegramReply = [
-    'chat_id' => '39310553',
-    'text' => 'This is a reply',
-];
+if(isset($input['entry'][0]['messaging'][0]['message']['text'])) {
+    // Message is from Facebook
+    $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.getenv('FACEBOOK_PAGE_ACCESS_TOKEN');
+    $reply = [
+        'messaging_type' => 'RESPONSE',
+        'recipient' => [
+            'id' => '1615290031920309',
+        ],
+        'message' => [
+            'text' => 'This is a reply.',
+        ],
+    ];
+} else {
+    $url = 'https://api.telegram.org/bot'.getenv('TELEGRAM_TOKEN').'/sendmessage';
+    $reply = [
+        'chat_id' => '39310553',
+        'text' => 'This is a reply',
+    ];
+}
 
-$facebookReply = [
-    'messaging_type' => 'RESPONSE',
-    'recipient' => [
-        'id' => '1615290031920309',
-    ],
-    'message' => [
-        'text' => 'This is a reply.',
-    ],
-];
 
-$url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.getenv('FACEBOOK_PAGE_ACCESS_TOKEN');
-//$url = 'https://api.telegram.org/bot'.getenv('TELEGRAM_TOKEN').'/sendmessage';
 $ch = curl_init($url);
 
 // Tell cURL to send POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
 // Attach JSON string to post fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($facebookReply));
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($reply));
 // Set the content type
 curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
 // Execute
